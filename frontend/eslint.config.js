9cb1835169c7040e7ea4936f250865157235a7f3
xiao-e-yun <a0970323059@gmail.com>
2025-03-02 17:25:23 +0800

    add lint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import typescriptEslint from "typescript-eslint";
+
+export default typescriptEslint.config(
+  { ignores: ["*.d.ts", "**/coverage", "**/dist"] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      ...eslintPluginVue.configs["flat/recommended"],
+    ],
+    files: ["**/*.{ts,vue}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    rules: {},
+  },
+  {
+    files: ["src/pages/**/*.vue"],
+    rules: {
+      "vue/multi-word-component-names": 0,
+    },
+  },
+  eslintConfigPrettier,
+);
