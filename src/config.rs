699b24a9c6c7e5f4aad45cce23747191ebf5aa0e
xiaoeyun <67814228+xiao-e-yun@users.noreply.github.com>
2025-04-18 09:42:58 +0800

    Update config.rs
    Fix typos

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,7 @@ use ts_rs::TS;
 
 #[derive(Debug, Clone, Deserialize, Parser)]
 pub struct Config {
-    #[clap(env = "ARCHIVER_PATH", default_value = "archiver")]
+    #[clap(env = "ARCHIVER_PATH", default_value = "archive")]
     pub path: PathBuf,
     #[clap(long, default_value = "3000")]
     pub port: u16,
