7670b55155bdb166008160fdb0da38a62a9f54b6
xiao-e-yun <a0970323059@gmail.com>
2025-10-20 03:21:13 +0800

    style: animate

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -8,27 +8,25 @@ import {
   UsersRound,
 } from "lucide-vue-next";
 import { RouterLink } from "vue-router";
-import { useLazyLoad } from "@/lazyload";
-import { useTemplateRef, watch } from "vue";
 import PostList from "@/components/PostList.vue";
 import { Separator } from "@/components/ui/separator";
 import CategoryList from "@/components/CategoryList.vue";
-
-const authorsEl = useTemplateRef<HTMLDialogElement>("authorsList");
-watch(authorsEl, (el) => el && useLazyLoad().update());
 
 const RANDOM_QUERY = { order_by: "random" };
 </script>
 
 <template>
-  <RouterLink to="/authors" class="block mb-8">
+  <RouterLink to="/authors" class="block mb-8 group">
     <div class="flex justify-between">
       <h1 class="text-base lg:text-2xl">
         <UsersRound :size="30" class="inline-block mr-2" />
         Authors
       </h1>
 
-      <ChevronRight :size="30" />
+      <ChevronRight
+        :size="30"
+        class="group-hover:translate-x-1 transition-transform"
+      />
     </div>
     <Separator class="my-2" />
     <CategoryList
@@ -40,27 +38,33 @@ const RANDOM_QUERY = { order_by: "random" };
     />
   </RouterLink>
 
-  <RouterLink to="/posts" class="block mb-8">
+  <RouterLink to="/posts" class="block mb-8 group">
     <div class="flex justify-between">
       <h1 class="text-base lg:text-2xl">
         <Newspaper :size="30" class="inline-block mr-2" />
         Posts
       </h1>
 
-      <ChevronRight :size="30" />
+      <ChevronRight
+        :size="30"
+        class="group-hover:translate-x-1 transition-transform"
+      />
     </div>
     <Separator class="my-2" />
     <PostList url="/api/posts" :controls="false" :limit="5" inline />
   </RouterLink>
 
-  <RouterLink to="/tags" class="block mb-8">
+  <RouterLink to="/tags" class="block mb-8 group">
     <div class="flex justify-between">
       <h1 class="text-base lg:text-2xl">
         <Tags :size="30" class="inline-block mr-2" />
         Tags
       </h1>
 
-      <ChevronRight :size="30" />
+      <ChevronRight
+        :size="30"
+        class="group-hover:translate-x-1 transition-transform"
+      />
     </div>
     <Separator class="my-2" />
     <CategoryList
@@ -72,14 +76,17 @@ const RANDOM_QUERY = { order_by: "random" };
     />
   </RouterLink>
 
-  <RouterLink to="/collections" class="block mb-8">
+  <RouterLink to="/collections" class="block mb-8 group">
     <div class="flex justify-between">
       <h1 class="text-base lg:text-2xl">
         <Folders :size="30" class="inline-block mr-2" />
         Collections
       </h1>
 
-      <ChevronRight :size="30" />
+      <ChevronRight
+        :size="30"
+        class="group-hover:translate-x-1 transition-transform"
+      />
     </div>
     <Separator class="my-2" />
     <CategoryList
@@ -91,14 +98,17 @@ const RANDOM_QUERY = { order_by: "random" };
     />
   </RouterLink>
 
-  <RouterLink to="/platforms" class="block mb-12">
+  <RouterLink to="/platforms" class="block mb-12 group">
     <div class="flex justify-between">
       <h1 class="text-base lg:text-2xl">
         <Quote :size="30" class="inline-block mr-2" />
         Platforms
       </h1>
 
-      <ChevronRight :size="30" />
+      <ChevronRight
+        :size="30"
+        class="group-hover:translate-x-1 transition-transform"
+      />
     </div>
     <Separator class="my-2" />
     <CategoryList category="platforms" :controls="false" :limit="12" inline />
