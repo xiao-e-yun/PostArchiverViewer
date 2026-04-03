9d67db8f90b06d78c38d56c2043c34aba503e6d4
xiao-e-yun <a0970323059@gmail.com>
2026-04-03 08:53:01 +0800

    feat: CategoryPosts has no placeholder

--- frontend/src/pages/CategoryPosts.vue
+++ frontend/src/pages/CategoryPosts.vue
@@ -6,6 +6,7 @@ import DynamicImage from "@/components/image/DynamicImage.vue";
 import PostSearch from "@/components/search/PostSearch.vue";
 import type { SearchQuerys } from "@/components/search/search";
 import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useLazyLoad } from "@/lazyload";
 import { useFetchWithCache, useRelations } from "@/utils";
 import type { WithRelations } from "@api/WithRelations";
@@ -42,25 +43,36 @@ const relations = useRelations(data);
         <h1 class="text-4xl underline underline-offset-8">{{ data.name }}</h1>
         <h1 class="text-lg mt-1">{{ props.category }}</h1>
       </RouterLink>
-      <div class="flex justify-end gap-2 w-full">
+      <div class="flex justify-end gap-2 w-full h-6">
         <AuthorAliases v-if="props.category === 'authors'" :id="id" />
         <Badge v-if="'source' in data!">
           <a v-if="data.source" :href="data.source"> Source </a>
         </Badge>
       </div>
+    </template>
+    <template v-else>
+      <div class="ml-4 flex flex-col gap-2">
+        <Skeleton class="h-10 w-48" />
+        <Skeleton class="h-6 w-20" />
+      </div>
 
-      <div
-        v-if="'thumb' in data && data.thumb"
-        class="absolute -bottom-4 md:w-[calc(100%+4rem)] w-[calc(100%+2rem)] max-w-none -left-4 md:-left-8 -z-10 overflow-hidden border-b h-[calc(100%+3rem)]"
-      >
-        <DynamicImage
-          :width="10"
-          :src="relations.fileMetaPath(data.thumb)!"
-          class="object-cover object-center inset-0 w-full h-full scale-110 blur-md opacity-0 [&.loaded]:opacity-50 duration-300"
-          @vue:mounted="() => useLazyLoad().update()"
-        />
+      <div class="flex justify-end gap-2 w-full">
+        <Skeleton class="h-6 w-20 rounded-full" />
+        <Skeleton class="h-6 w-24 rounded-full" />
       </div>
     </template>
+
+    <div
+      class="absolute -bottom-4 md:w-[calc(100%+4rem)] w-[calc(100%+2rem)] max-w-none -left-4 md:-left-8 -z-10 overflow-hidden border-b h-[calc(100%+3rem)]"
+    >
+      <DynamicImage
+        v-if="data && 'thumb' in data && data.thumb"
+        :width="10"
+        :src="relations.fileMetaPath(data.thumb)!"
+        class="object-cover object-center inset-0 w-full h-full scale-110 blur-md opacity-0 [&.loaded]:opacity-50 duration-300"
+        @vue:mounted="() => useLazyLoad().update()"
+      />
+    </div>
   </div>
   <PostSearch :defaults="{ [category]: [id] }" />
 </template>
